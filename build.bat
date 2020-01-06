aacf732db24391422c2da449e0df0f77219cbd28
AimlessWanderer <mars873@gmail.com>
2020-01-07 00:44:45 +0200

    new structure

--- build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-SetLocal EnableExtensions EnableDelayedExpansion
-color 09
-title Compressing. Please wait... [Approximately 1 min]
-cd pk3_data
-..\7z a -t7z -m0=lzma2 -mx=9 -aoa "..\DPT-HQ-MapsAddon-%date:~6,4%%date:~3,2%%date:~0,2%-%time:~0,2%%time:~3,2%.pk7" *
-cls
-echo Compression complete.
-pause
